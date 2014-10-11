4409843d49028ce34516a811f9549ae800400791
Ton <ton@zaqqle.com>
2014-10-11 20:10:08 +0700

    refactor views and add delete button

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,6 +9,16 @@ class TweetsController < BackendController
     end
   end
 
+  def destroy
+    tweet = current_user.tweets.find(params[:id])
+    if tweet.destroy
+      redirect_to [:timeline, :index], notice: 'Status has been deleted.'
+    else
+      flash[:error] = "Something went wrong."
+      redirect_to [:timeline, :index]
+    end
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:tweet)
